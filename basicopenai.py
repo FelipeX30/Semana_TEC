b814da11edc61c0271b9ceedff1f5e07546a3e93
Felipe Barbosa <a01784465@tec.mx>
2024-10-21 12:52:02 -0600

    initial

---
+++ basicopenai.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+
+secret_key = ""
+prompt = "What is the capital of the United States?"
+
+client = OpenAI(
+    api_key=secret_key
+    )
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+  ],
+  max_tokens=100,
+  temperature=1
+)
+
+print(completion)
